83e6037b548732bda2d3fc3198355e4c41c5d5d7
ManuKumar.B <manukumar38@gmail.com>
2024-02-02 00:28:06 +0530

    first commit

--- src/com/recurssion/problems/StringPowerSet.java
+++ src/com/recurssion/problems/StringPowerSet.java
@@ -3,19 +3,21 @@ package com.recurssion.problems;
 public class StringPowerSet {
 
     static void abc(String input, String output) {
-        if (input.length() == 0) {
+        if (input.isEmpty()) {
             System.out.println(output);
             return;
         }
 
+        //don't pick
+        abc(input.substring(1), output);
+
         //pick
         abc(input.substring(1), output+input.charAt(0));
 
-        //don't pick
-        abc(input.substring(1), output);
     }
 
     public static void main(String[] args) {
+
         abc ("abc", "");
     }
 }
