7499c2d31d118df882fe4a03c8bb9a04ec1fea99
ManuKumar.B <manukumar38@gmail.com>
2024-03-22 16:52:39 +0530

    first commit

--- src/com/leetcode/solutions/ArrayProblem.java
+++ src/com/leetcode/solutions/ArrayProblem.java
@@ -1,31 +1,24 @@
 package com.leetcode.solutions;
 
 public class ArrayProblem {
-
-
-    // to move all 0 to end
     public static void main(String[] args) {
-        int[] arr = {1, 2, 0, 4, 3, 0, 5, 0};
 
-        int check0 = 0;
-        for(int i = 0; i < arr.length; i++) {
-            if (arr[i] == 0){
-                check0++;
-            }
-        }
-
+        int[] arr = {1,0,3,4,0,6,7,0,4,0,6};
         int index = 0;
-        for(int i = 0; i < arr.length; i++) {
+
+        for(int i=0; i < arr.length; i++) {
             if (arr[i] != 0){
-                arr[index++] = arr[i];
+                arr[index] = arr[i];
+                index++;
             }
         }
-        while(check0 > 0){
-            arr[index++] = 0;
-            check0--;
-        }
 
-        for(int i = 0; i < arr.length; i++) {
+        for (int i = index; i < arr.length ; i++) {
+            arr[i] = 0;
+        }
+        System.out.println("index : "+index);
+        for(int i = 0; i < arr.length; i++){
+
             System.out.println(arr[i]);
         }
     }
